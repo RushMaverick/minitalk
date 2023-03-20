5bdac00c3de3546550cdd26ab084824ded5aa8b1
Rasmus Rask <rasse.rask@gmail.com>
2023-03-20 17:54:02 +0200

    Working on bitshifting

--- src/client.c
+++ src/client.c
@@ -2,6 +2,15 @@
 #include <signal.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include "libft.h"
+
+static void *handle_string(int pid, char *msg)
+{
+	int bit;
+	bit = 0x10000000;
+
+	// while ();
+}
 
 int main (int argc, char *argv[])
 {
@@ -10,7 +19,7 @@ int main (int argc, char *argv[])
 		printf("%s\n", "dude no");
 		return (0);
 	}
-	kill(atoi(argv[1]), SIGUSR1); //Use to send signal to the PID
+	handle_string(ft_atoi(argv[1]), argv[2]);
 	
 	return 0;
 }
